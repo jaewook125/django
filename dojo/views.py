6e8b2aec5f658e68f22ba6e90b4c6d43b83e4b14
jaewook125 <sjw0484@naver.com>
2017-03-24 18:29:06 +0900

    dojo.views.hello : 한글 문자열 매칭

--- dojo/views.py
+++ dojo/views.py
@@ -6,6 +6,8 @@ def mysum(request, numbers):
     # request: HttpResponse
     # numbers = "1/2/123/123/132/1215/123123"
     result = sum(map(lambda s: int(s or 0), numbers.split("/")))
+
     return HttpResponse(result)
 
-# Create your views here.
+def hello(request, name, age):
+    return HttpResponse('안녕하세요. {}. {}살이시네요.'.format(name, age))
