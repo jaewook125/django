b61f174425e8f70d17edefedf58a79455a2639ef
jaewook125 <sjw0484@naver.com>
2017-04-10 14:47:03 +0900

    프로필 뷰에 login_required 장식자 추가

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 #accounts/views.py
 from django.conf import settings
 from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 
 
@@ -16,6 +17,8 @@ def signup(request):
         'form': form
     })
 
-
+@login_required
 def profile(request):
     return render(request, 'accounts/profile.html')
+#특정 뷰가 호출될때 로그아웃 상황이 아닌
+#로그인상황에서만 호출이 되도록 강제시킬려면 장식자를 사용 @login_required
